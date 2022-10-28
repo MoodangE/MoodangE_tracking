a9267213207bfe75c85e423a88868139f27a3ea0
woo980711@naver.com <woo980711@naver.com>
2022-10-28 20:30:47 +0900

    [10/28 Update] 10/28 gachon_road, wandb name

--- yolov5/train.py
+++ yolov5/train.py
@@ -72,7 +72,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
     callbacks.run('on_pretrain_routine_start')
 
     wandb.init(project="MoodangE_tracking", entity="moodange")
-    wandb.run.name = '(10/11)_Aug_Rotation'
+    wandb.run.name = '(10/28)_Augmentations'
     wandb.run.save()
 
     # Directories
