8d4da38645b36f588e3b35238527de203a2f72c7
Chung Hyunwoo <woo980711@naver.com>
2022-10-29 01:32:44 +0900

    [10/28 Update] add: _cam 파일에서 결과 저장 과정을 제외한 파일

--- tracking_location.py
+++ tracking_location.py
@@ -30,6 +30,12 @@ torch.set_printoptions(precision=3)
 
 palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
 
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # MoodangE_tracking root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
 
 def bbox_rel(*xyxy):
     """" Calculates the relative bounding box from absolute pixel values. """
@@ -52,7 +58,7 @@ def compute_color_for_labels(label):
     return tuple(color)
 
 
-def draw_boxes(img, bbox, identities=None, categories=None, names=None, offset=(0, 0)):
+def scene_boxes(img, bbox, identities=None, categories=None, names=None, offset=(0, 0)):
     for i, box in enumerate(bbox):
         x1, y1, x2, y2 = [int(i) for i in box]
         x1 += offset[0]
@@ -73,37 +79,29 @@ def draw_boxes(img, bbox, identities=None, categories=None, names=None, offset=(
             img, (x1, y1), (x1 + t_size[0] + 3, y1 + t_size[1] + 4), color, -1)
         cv2.putText(img, label, (x1, y1 +
                                  t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 2, [255, 255, 255], 2)
+        print('\t', label)
     return img
 
 
 @torch.no_grad()
 def run(
-        out='inference/output',
-        weights='yolov5/yolov5s.pt',  # model.pt path(s)
-        source='yolov5/data/images',  # file/dir/URL/glob, 0 for webcam
-        data='yolov5/models/yolov5s.yaml',  # customDataset.yaml path
+        weights=ROOT / 'yolov5/yolov5s.pt',  # model.pt path(s)
+        source=ROOT / 'yolov5/data/images',  # file/dir/URL/glob, 0 for webcam
+        data=ROOT / 'yolov5/models/yolov5s.yaml',  # customDataset.yaml path
         imgsz=(640, 640),  # inference size (height, width)
         conf_thres=0.25,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        view_img=False,  # show results
-        save_txt=False,  # save results to *.txt
-        save_conf=False,  # save confidences in --save-txt labels
-        save_crop=False,  # save cropped prediction boxes
         nosave=False,  # do not save images/videos
         classes=None,  # filter by class: --class 0, or --class 0 2 3
         agnostic_nms=False,  # class-agnostic NMS
         augment=False,  # augmented inference
         visualize=False,  # visualize features
-        update=False,  # update all models
-        project='runs/detect',  # save results to project/name
+        project=ROOT / 'inference_location',  # save results to project/name
         name='exp',  # save results to project/name
         exist_ok=False,  # existing project/name ok, do not increment
         line_thickness=3,  # bounding box thickness (pixels)
-        hide_labels=False,  # hide labels
-        hide_conf=False,  # hide confidences
-        half=False,  # use FP16 half-precision inference
         dnn=False,  # use OpenCV DNN for ONNX inference
 
         sort_max_age=5,
@@ -125,13 +123,11 @@ def run(
                         iou_threshold=sort_iou_thresh)  # {plug into parser}
 
     # Directory and CUDA settings for YOLOv5
-    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
-    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)
-
     device = select_device(device)
-    if os.path.exists(out):
-        shutil.rmtree(out)  # delete output folder
-    os.makedirs(out)  # make new output folder
+    save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
+    if os.path.exists(save_dir):
+        shutil.rmtree(save_dir)  # delete output folder
+    os.makedirs(save_dir)  # make new output folder
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load YOLOv5 model
@@ -141,14 +137,12 @@ def run(
 
     # Set Dataloader
     if webcam:
-        view_img = check_imshow()
         cudnn.benchmark = True  # set True to speed up constant image size inference
         dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt)
         bs = len(dataset)  # batch_size
     else:
         dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt)
         bs = 1  # batch_size
-    vid_path, vid_writer = [None] * bs, [None] * bs
 
     # Run inference
     model.warmup(imgsz=(1 if pt else bs, 3, *imgsz))  # warmup
@@ -187,10 +181,7 @@ def run(
 
             p = Path(p)  # to Path
             save_path = str(save_dir / p.name)  # im.jpg
-            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
             s += '%gx%g ' % im.shape[2:]  # print string
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-            imc = im0.copy() if save_crop else im0  # for save_crop
             annotator = Annotator(im0, line_width=line_thickness, example=str(names))
 
             # Rescale boxes from img_size (temporarily downscaled size) to im0 (native) size
@@ -207,113 +198,44 @@ def run(
             # NOTE: We send in detected object class too
             for x1, y1, x2, y2, conf, detclass in det.cpu().detach().numpy():
                 dets_to_sort = np.vstack((dets_to_sort, np.array([x1, y1, x2, y2, conf, detclass])))
-            print('\n')
-            print('Input into SORT:\n', dets_to_sort, '\n')
 
             # Run SORT
             tracked_dets = sort_tracker.update(dets_to_sort)
-
-            print('Output from SORT:\n', tracked_dets, '\n')
 
             # draw boxes for visualization
             if len(tracked_dets) > 0:
                 bbox_xyxy = tracked_dets[:, :4]
                 identities = tracked_dets[:, 8]
                 categories = tracked_dets[:, 4]
-                draw_boxes(im0, bbox_xyxy, identities, categories, names)
-
-            # Write detections to file. NOTE: Not MOT-compliant format.
-            if save_txt and len(tracked_dets) != 0:
-                for j, tracked_dets in enumerate(tracked_dets):
-                    bbox_x1 = tracked_dets[0]
-                    bbox_y1 = tracked_dets[1]
-                    bbox_x2 = tracked_dets[2]
-                    bbox_y2 = tracked_dets[3]
-                    category = tracked_dets[4]
-                    u_overdot = tracked_dets[5]
-                    v_overdot = tracked_dets[6]
-                    s_overdot = tracked_dets[7]
-                    identity = tracked_dets[8]
-
-                    with open(txt_path, 'a') as f:
-                        f.write(
-                            f'{frame_idx},{bbox_x1},{bbox_y1},{bbox_x2},{bbox_y2},{category},{u_overdot},{v_overdot},{s_overdot},{identity}\n')
-
-            print(f'{s} Done. ({t2 - t1})')
-            # Stream results
-            im0 = annotator.result()
-            if view_img:
-                if platform.system() == 'Linux' and p not in windows:
-                    windows.append(p)
-                    cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
-                    cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
-
-            # Save results (image with detections)
-            if save_img:
-                if dataset.mode == 'image':
-                    cv2.imwrite(save_path, im0)
-                else:  # 'video' or 'stream'
-                    if vid_path[i] != save_path:  # new video
-                        vid_path[i] = save_path
-                        if isinstance(vid_writer[i], cv2.VideoWriter):
-                            vid_writer[i].release()  # release previous video writer
-                        if vid_cap:  # video
-                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        else:  # stream
-                            fps, w, h = 30, im0.shape[1], im0.shape[0]
-                        save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
-                        vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-                    vid_writer[i].write(im0)
-
-        # Print time (inference-only)
-        LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
-
-    # Print results
-    t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
-    LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
-    if save_txt or save_img:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
-    if update:
-        strip_optimizer(weights[0])  # update model (to fix SourceChangeWarning)
+                scene_boxes(im0, bbox_xyxy, identities, categories, names)
+            total_duration = time_sync() - t3
+            print('\tTime taken per frame: {:.4f}\n'.format(total_duration))
 
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--out', type=str, default='inference/output',
-                        help='output folder')  # output folder
 
     # YOLOv5 params
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov5/yolov5s.pt', help='model path(s)')
-    parser.add_argument('--source', type=str, default='yolov5/data/images', help='file/dir/URL/glob, 0 for webcam')
-    parser.add_argument('--data', type=str, default='yolov5/data/coco128.yaml', help='(optional) customDataset.yaml path')
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5/yolov5s.pt', help='model path(s)')
+    parser.add_argument('--source', type=str, default=ROOT / 'yolov5/data/images',
+                        help='file/dir/URL/glob, 0 for webcam')
+    parser.add_argument('--data', type=str, default=ROOT / 'yolov5/data/coco128.yaml',
+                        help='(optional) customDataset.yaml path')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640],
                         help='inference size h,w')
     parser.add_argument('--conf-thres', type=float, default=0.3, help='confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.4, help='NMS IoU threshold')
     parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true', help='show results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
     parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
     parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument('--visualize', action='store_true', help='visualize features')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--project', default=ROOT / 'inference_location', help='save results to project/name')
     parser.add_argument('--name', default='exp', help='save results to project/name')
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
-    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
-    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
     parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
 
     # SORT params
